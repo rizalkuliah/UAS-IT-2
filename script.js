8cb2cfbb29222d4028c37c7134983ff5dfda50ae
Rizal gunder <naufalrizalagn@gmail.com>
2026-07-15 16:54:34 +0700

    menambah data produk default dan sistem versioning Memastikan semua pengguna mendapatkan data produk terbaru ketika developer mengubah konten (gambar, harga, stok, dll).

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-Struktur
+const DATA_VERSION = 3; // <-- Ubah angka ini (misal jadi 3) setelah ganti gambar
+
+const DEFAULT_PRODUCTS = [
+    {id:1, name:'Classic White Tee', category:'Casual', price:299000, promo:0, sizes:{S:8,M:12,L:10,XL:5}, image:'image/Clasicwhite.png'},
+    {id:2, name:'Black Essential', category:'Basic', price:249000, promo:0, sizes:{S:10,M:15,L:8,XL:4}, image:'image/Black.png'},
+    {id:3, name:'Navy Blue Stripe', category:'Casual', price:349000, promo:0, sizes:{S:6,M:10,L:8,XL:3}, image:'image/navyblue.png'},
+    {id:4, name:'Forest Green', category:'Basic', price:279000, promo:0, sizes:{S:8,M:12,L:6,XL:4}, image:'image/green.png'},
+    {id:5, name:'Vintage Graphic', category:'Graphic', price:399000, promo:0, sizes:{S:4,M:6,L:4,XL:2}, image:'image/vintage graphic.png'},
+    {id:6, name:'Sport Performance', category:'Sport', price:449000, promo:0, sizes:{S:6,M:8,L:6,XL:3}, image:'image/sports.png'},
+    {id:7, name:'Premium Cotton', category:'Premium', price:499000, promo:0, sizes:{S:4,M:5,L:4,XL:2}, image:'image/premium.png'},
+    {id:8, name:'Limited Edition', category:'Limited', price:599000, promo:0, sizes:{S:2,M:3,L:2,XL:1}, image:'image/limited.png'},
+    {id:9, name:'Sunset Orange', category:'Casual', price:329000, promo:0, sizes:{S:6,M:10,L:6,XL:4}, image:'image/sunset.png'},
+    {id:10, name:'Grey Classic', category:'Basic', price:229000, promo:0, sizes:{S:10,M:14,L:10,XL:6}, image:'image/Grey.png'},
+    {id:11, name:'Tie Dye Wave', category:'Graphic', price:429000, promo:0, sizes:{S:3,M:5,L:3,XL:2}, image:'image/tie.png'},
+    {id:12, name:'Athletic Blue', category:'Sport', price:379000, promo:0, sizes:{S:5,M:8,L:5,XL:3}, image:'image/blue.png'}
+];
