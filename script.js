12466bfcff3ef9eb8fbd18f6f75463536904b8b2
rizalkuliah <buattugaskuliah51@gmail.com>
2026-07-15 20:34:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -295,7 +295,7 @@ function openModal(id) {
     document.getElementById('modalName').textContent = p.name;
     const price = p.promo && p.promo > 0 ? p.promo : p.price;
     document.getElementById('modalPrice').innerHTML = fmt(price) + (p.promo && p.promo > 0 ? ` <span style="text-decoration:line-through;font-size:16px;color:var(--text-light);">${fmt(p.price)}</span>` : '');
-    document.getElementById('modalDesc').textContent = p.description || 'Kaos Berbahan Premium dari Katun yang menyerap keringat dan nyaman dipakai.';
+    document.getElementById('modalDesc').textContent = p.description || 'Kaos Berbahan Premium dari Katun yang menyerap keringat dan nyaman dipakai./Premium cotton T-shirt that is sweat-absorbent and comfortable to wear.';
     document.getElementById('modalStock').textContent = getTotalStock(p) <= 0 ? '❌ Out of Stock' : `✅ ${getTotalStock(p)} total stock`;
     document.getElementById('modalStock').style.color = getTotalStock(p) <= 0 ? '#dc2626' : '#059669';
     const sizeContainer = document.getElementById('modalSizeButtons');
